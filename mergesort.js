1e144ab07d8e49a5286e51a7069cacaeb0a823be
Kevin Delvo <kevinbdelvo@gmail.com>
2018-04-23 17:07:08 -0400

    ---

--- mergesort.js
+++ mergesort.js
@@ -1,4 +1,24 @@
 function split(wholeArray) {
+  if (wholeArray.length === 1) { //base case
+    return wholeArray;
+  }
+  let middle = Math.floor(wholeArray.length / 2);
+  let left = wholeArray.slice(0, middle);
+  let right = wholeArray.slice(middle);
+  console.log('left is ', left, 'right is ', right);
+  return sort(split(left), split(right));
+}
 
-  return [firstHalf, secondHalf];
+sort = (left, right) => {
+  let newArr = [];
+  while (left.length) {
+    if (left[0] > right[0]) {
+      newArr.push(right.shift());
+    } else {
+      newArr.push(left.shift());
+    }
+  }
+  console.log('new arr = ', newArr);
+
+  return newArr.concat(left).concat(right);
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,7 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    // your code here
+    // expect(split([3,8,2,4,1,5,7,6])).toEqual([1,2,3,4,5,6,7,8]);
+    expect(split([38,27,43,3,9,82,10])).toEqual([3,9,10,27,38,43,82]);
   });
+
 });
